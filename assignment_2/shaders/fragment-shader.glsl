478b3f335b93291fd9e8a2ec3407ae03b325d288
siyual-park <kdpark0723@gmail.com>
2021-12-13 13:18:00 +0900

    refactoring

--- assignment_2/shaders/fragment-shader.glsl
+++ assignment_2/shaders/fragment-shader.glsl
@@ -1,49 +1,10 @@
 #version 330 core
 
-struct Material {
-    sampler2D diffuse;
-    sampler2D specular;
-    float shininess;
-};
-
-struct Light {
-    vec3 position;
-
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
-
-    float constant;
-    float linear;
-    float quadratic;
-};
-
-in vec3 FragPos;
 in vec3 Color;
-
-uniform vec3 viewPos;
-uniform Material material;
-uniform Light light;
 
 out vec4 FragColor;
 
 void main()
 {
-    vec3 ambient = light.ambient * Color;
-
-    vec3 lightDir = normalize(light.position - FragPos);
-    vec3 diffuse = light.diffuse * Color;
-
-    vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 specular = light.specular * Color;
-
-    float distance = length(light.position - FragPos);
-    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
-
-    ambient *= attenuation;
-    diffuse *= attenuation;
-    specular *= attenuation;
-
-    vec3 result = ambient + diffuse + specular;
-    FragColor = vec4(result, 1.0);
+    FragColor = vec4(Color, 1.0);
 }
