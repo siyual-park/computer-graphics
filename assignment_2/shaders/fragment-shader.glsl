7e60406c4ca79da511e3be83a1822e23e89c4a0a
siyual-park <kdpark0723@gmail.com>
2021-12-13 12:56:13 +0900

    refactoring

--- assignment_2/shaders/fragment-shader.glsl
+++ assignment_2/shaders/fragment-shader.glsl
@@ -1,9 +1,49 @@
 #version 330 core
 
-in vec3 fragmentColor;
+struct Material {
+    sampler2D diffuse;
+    sampler2D specular;
+    float shininess;
+};
 
-out vec3 color;
+struct Light {
+    vec3 position;
 
-void main(){
-    color = fragmentColor;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+
+    float constant;
+    float linear;
+    float quadratic;
+};
+
+in vec3 FragPos;
+in vec3 Color;
+
+uniform vec3 viewPos;
+uniform Material material;
+uniform Light light;
+
+out vec4 FragColor;
+
+void main()
+{
+    vec3 ambient = light.ambient * Color;
+
+    vec3 lightDir = normalize(light.position - FragPos);
+    vec3 diffuse = light.diffuse * Color;
+
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 specular = light.specular * Color;
+
+    float distance = length(light.position - FragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+
+    ambient *= attenuation;
+    diffuse *= attenuation;
+    specular *= attenuation;
+
+    vec3 result = ambient + diffuse + specular;
+    FragColor = vec4(result, 1.0);
 }
--- assignment_2/shaders/vertex-shader.glsl
+++ assignment_2/shaders/vertex-shader.glsl
@@ -3,7 +3,8 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 
-out vec3 fragmentColor;
+out vec3 FragPos;
+out vec3 Color;
 
 uniform mat4 model;
 uniform mat4 world;
@@ -12,6 +13,8 @@ uniform mat4 projection;
 
 void main()
 {
-    fragmentColor = aColor;
+    FragPos = vec3(world * model * vec4(aPos, 1.0));
+    Color = aColor;
+
     gl_Position = projection * view * world * model * vec4(aPos, 1.0);
 }
