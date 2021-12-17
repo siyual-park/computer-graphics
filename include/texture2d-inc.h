749660f52374175f87e64984759ca72b621fc6bd
siyual-park <kdpark0723@gmail.com>
2021-12-18 01:44:04 +0900

    fix error

--- include/texture2d-inc.h
+++ include/texture2d-inc.h
@@ -16,8 +16,8 @@ gl::Texture2d<T>::Texture2d(T *data, Texture2dSize size, int internal_format, in
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
     unbind();
 
