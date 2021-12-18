48ec72c1792b2afd895887147605adfd4473e557
siyual-park <kdpark0723@gmail.com>
2021-12-18 19:29:55 +0900

    add weight

--- shaders/raycasting.frag.glsl
+++ shaders/raycasting.frag.glsl
@@ -21,6 +21,16 @@ float calculate_step(vec3 normDir) {
     return pow(x + y + z, 0.5);
 }
 
+float calculate_weight(vec3 normDir) {
+    vec3 normSpacing = normalize(VolumeSpacing);
+
+    float x = pow(abs(normDir.x * normSpacing.x), 2);
+    float y = pow(abs(normDir.y * normSpacing.y), 2);
+    float z = pow(abs(normDir.z * normSpacing.z), 2);
+
+    return pow(x + y + z, 0.5);
+}
+
 void main() {
     vec2 exitFragCoord = (ExitPointCoord.xy / ExitPointCoord.w + 1.0f) / 2.0f;
     vec3 exitPoint = texture(ExitPoints, exitFragCoord).xyz;
@@ -38,6 +48,8 @@ void main() {
     float step = calculate_step(normDir);
     float stepSize = 1.0f / step;
 
+    float weight = calculate_weight(normDir);
+
     vec3 deltaDir = normDir * stepSize;
     float deltaDirLen = length(deltaDir);
 
@@ -48,12 +60,12 @@ void main() {
 
     vec4 backgoundColor = vec4(0.0f, 0.0f, 0.0f, 0.0f);
 
-    while (true) {
+    for (int i = 0; i < step * 2; i++) {
         float intensity = (texture(VolumeTex, voxelCoord).x + 1.0f) / 2.0f;
         vec4 colorSample = texture(TransferFunc, intensity);
 
         if (colorSample.a > 0.0f) {
-            colorSample.a = 1.0 - pow(1.0 - colorSample.a, stepSize * pow(step, 1.2f));
+            colorSample.a = 1.0 - pow(1.0 - colorSample.a, weight);
             colorAcum.rgb += (1.0f - colorAcum.a) * colorSample.rgb * colorSample.a;
             colorAcum.a += (1.0f - colorAcum.a) * colorSample.a;
         }
