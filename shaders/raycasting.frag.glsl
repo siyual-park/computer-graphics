430d9c8004f3680642edad666a4b31c601881eac
siyual-park <kdpark0723@gmail.com>
2021-12-21 23:33:40 +0900

    change norm vector

--- shaders/raycasting.frag.glsl
+++ shaders/raycasting.frag.glsl
@@ -95,7 +95,7 @@ vec3 calculateNormal(vec3 voxelCoord, vec3 unitVoxelSize) {
     float zFront = samplingForNormal(vec3(voxelCoord.x, voxelCoord.y, voxelCoord.z + unitVoxelSize.z));
     float zBack = samplingForNormal(vec3(voxelCoord.x, voxelCoord.y, voxelCoord.z - unitVoxelSize.z));
 
-    return vec3(xFront - xBack, yFront - yBack, zFront - zBack);
+    return vec3(xFront - xBack, yFront - yBack, zFront - zBack) * VolumeSpacing;
 }
 
 vec3 changeToWorldCoord(vec3 voxelCoord) {
